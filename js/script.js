a21779576e2351bb458020a234194a2b845a5cd5
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-05-26 00:35:44 +0400

    first commit
    add style

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let users = prompt('users');
+
+// async function githubusers() {
+//   let url = `https://api.github.com/users/${users}`;
+
+//   let response = await fetch(url);
+
+//   console.log('response:' + response);
+
+//   let data = await response.json();
+
+//   console.log(data);
+// }
+// githubusers();
