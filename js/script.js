debbf4d5df586ea676622a6691b5bb0dbd6b820d
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-05-28 03:18:49 +0400

    C
    V

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-// let users = prompt('users');
+let users = prompt('users');
 
-// async function githubusers() {
-//   let url = `https://api.github.com/users/${users}`;
+async function githubusers() {
+  let url = `https://api.github.com/users/${users}`;
 
-//   let response = await fetch(url);
+  let response = await fetch(url);
 
-//   console.log('response:' + response);
+  console.log('response:' + response);
 
-//   let data = await response.json();
+  let data = await response.json();
 
-//   console.log(data);
-// }
-// githubusers();
+  console.log(data);
+
+  if (data.message === 'Not Found') {
+    let noresult = document.querySelector('iput_box');
+
+    let noresulttxt = document.createElement('p');
+    noresulttxt.textContent = 'No results';
+    noresulttxt.classList.add('no_Esult');
+
+    console.log(noresulttxt);
+
+    noresult.appendChild(noresulttxt);
+  }
+}
+githubusers();
